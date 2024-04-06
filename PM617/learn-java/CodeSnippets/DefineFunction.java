e5fadb51f5087fdba00f9d68e8b7e3abe272df1e
colarm <haichzh@gmail.com>
2024-04-06 22:53:57 +0100

    Add Java course

---
+++ PM617/learn-java/CodeSnippets/DefineFunction.java
@@ -0,0 +1,11 @@
+// This is the starter code snippet. 
+// Follow the notebook to finish it up!
+
+public class DefineFunction {
+
+    public static void AnnounceDeveloperTeaTime();// Adding the label of our function
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ PM617/learn-java/CodeSnippets/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { //main function starts here
+  public static void main(String[] args){
+    System.out.println("Hello World! It's me, Grace!!");
+  }//End of main function
+}//End of hello Class
